c08076dfa5b4f90c36d981f887b37bd1426853f8
Eric Sheffield <ersheff@gmail.com>
2024-05-08 14:06:30 -0400

    asdf

--- script.js
+++ script.js
@@ -3,7 +3,13 @@ document.addEventListener("DOMContentLoaded", () => {
   if (room) {
     htmx.ajax("GET", "pages/" + room + ".html", "main");
     history.pushState({}, "", "?room=" + room);
+    document.getElementById("select-nav").value = room;
   } else {
     htmx.ajax("GET", "pages/home.html", "main");
+    document.getElementById("select-nav").value = "home";
   }
+});
+
+document.getElementById("logo-nav").addEventListener("click", () => {
+  document.getElementById("select-nav").value = "home";
 });
